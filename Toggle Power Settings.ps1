ac610753d6677d1f1947c6b9eda5e52a66af3a7a
Ray <48207561+Rayaxe2@users.noreply.github.com>
2024-03-24 02:26:41 +0000

    Add files via upload

---
+++ Toggle Power Settings.ps1
@@ -0,0 +1,9 @@
+﻿if((powercfg.exe /GETACTIVESCHEME) -Match "a1841308-3541-4fab-bc81-f71556f20b4a"){
+    powercfg.exe /s 9935e61f-1661-40c5-ae2f-8495027d5d5d
+}
+else {
+    powercfg.exe /s a1841308-3541-4fab-bc81-f71556f20b4a
+}
+echo (powercfg.exe /GETACTIVESCHEME)
+Start-Sleep -s 2
+Exit
